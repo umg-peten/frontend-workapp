fe488267508443813974d0d37cdc931edbac7085
Axelazo <59715526+Axelazo@users.noreply.github.com>
2021-10-17 21:42:20 -0600

    Primer commit

---
+++ js/scripts.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+
+    /* Carga el contenido del dashboard por defecto la primera vez */
+    //$('#dashboard-content').load('partials/dashboard.html')
+
+    /* Logica del menu sacado de aqui https://codepen.io/arjunamgain/pen/lGALt */
+    $('ul li a').click(function () {
+        $('li a').removeClass("active");
+        $(this).addClass("active");
+
+        /* Cargar contenido dinamicamente */
+        var href = $(this).attr('href');
+        var newHref = href.substring(1); // # removido
+
+        $('#dashboard-content').load('partials/' + newHref + '.html')
+
+    });
+
+
+    /***  Dashboard  ***/
+
+    /* Fecha y hora */
+/*     
+    var intervalId = setInterval(function () {
+        $("#datetime").text(date);
+    }, 1000); */
+
+    var myVar = setInterval(myTimer, 1000);
+    var date = new Date().toLocaleString();
+    function myTimer() {
+        $("#datetime").text(date);
+    }
+
+    /* Graficos*/
+    var ctx1 = document.getElementById('chart1').getContext('2d');
+    var myChart = new Chart(ctx1, {
+        type: 'pie',
+        data: {
+            labels: ['Activos', 'Inactivos', 'Licencia', 'Falta', 'En proceso (despido)'],
+            datasets: [{
+                label: '# de Empleados',
+                data: [45, 5, 3, 2, 1, 0],
+                backgroundColor: [
+                    'rgba(255, 99, 132)',
+                    'rgba(54, 162, 235)',
+                    'rgba(255, 206, 86)',
+                    'rgba(75, 192, 192)',
+                    'rgba(153, 102, 255)',
+                    'rgba(255, 159, 64)'
+                ],
+                borderWidth: 1
+            }]
+        }
+
+    });
+
+    /* Obtener los dias anteriores */
+    var days = [];
+    for (i = 1; i <= 7; i++) {
+        var day = moment().add(i, 'days');
+        day.locale('es');
+        days.push(day.format('dddd'));
+    }
+
+    /* Graficos #2 */
+    var ctx2 = document.getElementById('chart2').getContext('2d');
+    var myChart = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+            labels: days,
+            datasets: [{
+                label: '# de trabajadores que registraron asistencia',
+                data: [43, 43, 45, 44, 45, 45, 45],
+                backgroundColor: [
+                    'rgba(255, 99, 132)',
+                    'rgba(54, 162, 235)',
+                    'rgba(255, 206, 86)',
+                    'rgba(75, 192, 192)',
+                    'rgba(153, 102, 255)',
+                    'rgba(255, 159, 64)'
+                ],
+                borderWidth: 1
+            }]
+        }
+
+    });
+
+
+});
+
+
